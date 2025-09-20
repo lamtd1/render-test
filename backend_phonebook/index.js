d40643a759339b5f7550860219314bc086b13dff
lamtd1 <taduylam1608@gmail.com>
2025-09-20 23:30:50 +0700

    uibuild

--- backend_phonebook/index.js
+++ backend_phonebook/index.js
@@ -28,86 +28,70 @@ app.use(requestLogger)
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-// const genId = () => {
-//     return String(Math.floor(Math.random() * 10000))
-// }
-
-
 const now = new Date()
 
 app.get('/api/persons', (req, res) => {
     Phone.find({}).then(phones => res.json(phones))
 })
 
-app.get('/info', (req, res) => {
-    res.send(
-        `<p>Phone book has info for ${persons.length} people</p><p>${now}</p>`
+app.get('/info', (req, res, next) => {
+    Phone.countDocuments({}).then(
+      count => res.send(`<p>Phone book has info for ${count} people</p><p>${now}</p>`
     )
+    )
+
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    Phone.findById(id).then(
-        phone => res.json(phone)
+app.get('/api/persons/:id', (req, res, next) => {
+    Phone.findById(req.params.id).then(
+      person => res.json(person)
+    ).catch(
+      err => next(err)
     )
-})
+} )
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-      Phone.findByIdAndDelete(id).then(
-        deletedPhone => {
-          if (!deletedPhone) {
-            return res.status(404).json({error: "note not found"})
-          }
-          res.status(204).end()
-        }
-      )
-    })
-
-app.post('/api/persons', (req, res) => {
-    const body = req.body
-    if (!body.name || !body.number){
-        return res.status(400).json({"err": 'missing name or body'})
+app.delete('/api/persons/:id', (req, res, next) => {
+  Phone.findByIdAndDelete(req.params.id).then(
+    person => {
+      if(!person) {
+        return res.status(404).json({err: 'person not exist'})
+      } else {
+        return res.status(204).end()
+      }
     }
+  ).catch(err => next(err))
+})
 
-    Phone.findOne({name: body.name}).then(existing => {
-        if(existing){
-            return res.status(400).json({ err: 'name must be unique' })
-        }
+app.post('/api/persons', (req, res, next) => {
+  const {name, number} = req.body
+  if (!name || !number) {
+    return res.status(400).json({ error: 'content missing' })
+  }
+  const newPerson = new Phone({
+    name: name,
+    number: number
+  })
 
-        const newPerson = new Phone({
-            name: body.name,
-            number: body.number
-        })
+  newPerson.save().then(savedPerson => {
+    res.json(savedPerson)
+  }).catch(
+    err => next(err)
+  )
+})
 
-        newPerson.save().then(savedPerson => res.json(savedPerson))
-    })
-
-
-
+app.put('/api/persons/:id', (req, res, next)=> {
+  const {name, number} = req.body
+  Phone.findById(req.params.id).then(
+    person => {
+      if(!person) {
+        return res.status(404).json({err: "Person not exists"})
+      } else {
+        person.name = name
+        person.number = number
+        person.save().then(savedPerson => res.json(savedPerson))
+      }
+    }
+  ).catch(err => next(err))
 })
 
 const unknownEndpoint = (req, res) => {
@@ -115,6 +99,19 @@ const unknownEndpoint = (req, res) => {
 }
 
 app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
