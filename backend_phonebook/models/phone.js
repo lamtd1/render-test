dbb94a74985db8a86a35464f33db71c120bce60d
lamtd1 <taduylam1608@gmail.com>
2025-09-20 22:18:29 +0700

    uibuild

--- backend_phonebook/models/phone.js
+++ backend_phonebook/models/phone.js
@@ -1,23 +1,24 @@
 // Lấy dữ liệu từ cơ sở dữ liệu
 // Tách riêng phần logic lấy từ DB vào module riêng
 const mongoose = require('mongoose')
-
+// Giữ tính chất cữ
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to ', url)
+// kết nối mongoose
 mongoose.connect(url).then(
     result => console.log('connected')
 ).catch(err => {
     console.log('err connecting to DB: ', err.message)
 })
-
+// định nghĩa schema
 const phoneSchema = new mongoose.Schema({
     name: String,
     number: String
 })
-
+// chỉnh lại json trả về 
 phoneSchema.set('toJSON', {
     transform: (doc, ret) => {
         ret.id = ret._id.toString()
